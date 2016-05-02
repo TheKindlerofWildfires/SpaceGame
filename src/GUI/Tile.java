02fb60d2daa65bcaef66f64dfbc67ebf9b9aa214
hmsrothman <Simon@Simons-MacBook-Pro.local>
2016-05-02 14:53:58 -0500

    this was meant to be part of the last one :)

---
+++ src/GUI/Tile.java
@@ -0,0 +1,26 @@
+package GUI;
+
+public class Tile {
+
+	private boolean land;
+	private double moisture;
+	private double elevation;
+	
+	public int xIndex;
+	public int yIndex;
+	
+	public Tile(int xIndex, int yIndex, double moisture, double elevation) {
+		this.moisture=moisture;
+		this.elevation=elevation;
+		this.xIndex=xIndex;
+		this.yIndex=yIndex;
+	}
+	
+	public void setLand(boolean land){
+		this.land = land;
+	}	
+	
+	public boolean isLand(){
+		return land;
+	}
+}
