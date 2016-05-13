d8f81f3a4449d39fa210c9bfbfa35b7583b652c3
Kindler <Mjhock99@gmail.com>
2016-05-12 19:48:33 -0500

    Less of a mess

--- src/GUI/Window.java
+++ src/GUI/Window.java
@@ -2,41 +2,25 @@ package GUI;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.glfw.GLFW.*;
-<<<<<<< HEAD
-=======
 import static org.lwjgl.system.MemoryUtil.*;
 import gameEngine.*;
 
 import java.awt.Graphics;
->>>>>>> parent of 78dce87... This commit works on Simon's computer
 
 import org.lwjgl.glfw.GLFWCursorPosCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
-
-<<<<<<< HEAD
 import gameEngine.EntityManager;
 
-public class Window {
-=======
 public class Window implements Runnable {
 	private Thread thread;
->>>>>>> parent of 78dce87... This commit works on Simon's computer
 	public boolean running = true;
 
 	private GLFWKeyCallback keyCallback;
 	private GLFWCursorPosCallback cursorCallback;
 
 	public Long window;
-
-<<<<<<< HEAD
-	EntityManager entityManager;
-
-	public static void main(String args[]) {
-		Window game = new Window();
-		game.run();
-=======
 	
 	EntityManager entityManager; 
 	public static void main(String args[]) {
@@ -48,8 +32,6 @@ public class Window implements Runnable {
 		running = true;
 		thread = new Thread(this, "SpaceGame");
 		thread.start();
-
->>>>>>> parent of 78dce87... This commit works on Simon's computer
 	}
 
 	public void init() {
@@ -58,13 +40,10 @@ public class Window implements Runnable {
 			System.exit(-1);
 		}
 		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-<<<<<<< HEAD
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-=======
->>>>>>> parent of 78dce87... This commit works on Simon's computer
 
 		window = glfwCreateWindow(1920, 1080, "SpaceGame", NULL, NULL);
 
--- src/graphicEngine/Utilities.java
+++ src/graphicEngine/Utilities.java
@@ -34,37 +34,29 @@ public class Utilities {
 
 	public static int loadShader(String filepath, int type) {
 		StringBuilder result = new StringBuilder();
-<<<<<<< HEAD
-		System.out.println(filepath);
-		try {
-=======
 		try{
->>>>>>> parent of 78dce87... This commit works on Simon's computer
 			BufferedReader reader = new BufferedReader(new FileReader(filepath));
 			String buffer = "";
 			while ((buffer = reader.readLine()) != null) {
 				result.append(buffer);
 				result.append("\n");
 			}
-<<<<<<< HEAD
 			System.out.println(result);
 			reader.close();
 
-		} catch (IOException e) {
-=======
 		}catch (IOException e){
->>>>>>> parent of 78dce87... This commit works on Simon's computer
 			System.err.println(e);
-		} finally{
-		}
-		int shaderID = glCreateShader(type);
-		glShaderSource(shaderID, result.toString());
-		glCompileShader(shaderID);
-		if (glGetShaderi(shaderID, GL_COMPILE_STATUS) == GL_FALSE) {
-			System.err.println(glGetShaderInfoLog(shaderID, 500));
-			System.err.println("Could not compile shader");
-			System.err.println(-1);
-		}
+		} 
+		finally{
+			int shaderID = glCreateShader(type);
+			glShaderSource(shaderID, result.toString());
+			glCompileShader(shaderID);
+			if (glGetShaderi(shaderID, GL_COMPILE_STATUS) == GL_FALSE) {
+				System.err.println(glGetShaderInfoLog(shaderID, 500));
+				System.err.println("Could not compile shader");
+				System.err.println(-1);
+			}
 		return shaderID;
-	}
+		}
+		}
 }
