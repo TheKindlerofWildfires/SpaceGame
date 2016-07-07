5fd9a7e8e65dce794a7121aab3bb0c69c88f8f06
Kindler <Mjhock99@gmail.com>
2016-07-07 17:42:41 -0500

    Trying to make structures better

--- src/gen/Structure.java
+++ src/gen/Structure.java
@@ -14,165 +14,198 @@ public class Structure {
 	public static int sizeZ = 0;
 
 	public static void gen(int x, int y, int z) {
-		//this code is bad i think, should be single type per use
+		// this code is bad i think, should be single type per use
 		for (int i = 0; i < WorldType.interference.size(); i++) {
 			ruins[i] = WorldType.interference.get(i);
+			// x = Map.rng.nextInt(Map.HEXESACROSS);
+			// y = Map.rng.nextInt(Map.HEXESDOWN);
+			// z = Map.rng.nextInt(WorldGenerator.HEIGHT);
+			z = getTallest(x, y);
 			switch (ruins[i]) {
 			case "octividRuinsLeast":
-				octividRuinsLeast(x, y, z);
+				// octividRuinsLeast(x, y, z);
+				jeneunGateLeast(x, y, z);
 				break;
 			case "kinikariRuinsLeast":
-				kinikariRuinsLeast(x, y, z);
+				// kinikariRuinsLeast(x, y, z);
+				jeneunGateLeast(x, y, z);
 				break;
 			case "jeneunGateLeast":
 				jeneunGateLeast(x, y, z);
 				break;
 			case "jeneunRuinsLeast":
-				jeneunRuinsLeast(x, y, z);
+				// jeneunRuinsLeast(x, y, z);
+				jeneunGateLeast(x, y, z);
 				break;
 			}
-			
+
 		}
 	}
-	private static void clear(int cx, int cy, int cz){
-		//these "functions" may do literally jack shit
-		for(int q = cx; q<cx+sizeX; q++){
-			for(int w = cy; w<cy+sizeY; w++){
-				for(int e = cz; e<Map.WORLDHEIGHT; e++){
-					if(WorldGenerator.inBounds(q,w,e)){
-					WorldGenerator.data[q][w][e] = Block.AIR;
+
+	private static int getTallest(int x, int y) {
+		int tall = 0;
+		for (int q = x; q < x + sizeX; q++) {
+			for (int w = y; w < y + sizeY; w++) {
+				if (WorldGenerator.inBounds(q, w, 1)) {
+					if (WorldGenerator.eTracker[q][w] > tall) {
+						tall = WorldGenerator.eTracker[q][w];
 					}
 				}
 			}
 		}
+		return tall;
+	}
+
+	private static void clear(int cx, int cy, int cz) {
+		// this "function" may do literally jack shit
+		for (int q = cx; q < cx + sizeX; q++) {
+			for (int w = cy; w < cy + sizeY; w++) {
+				for (int e = cz; e < Map.WORLDHEIGHT; e++) {
+					if (WorldGenerator.inBounds(q, w, e)) {
+						WorldGenerator.data[q][w][e] = Block.AIR;
+					}
+				}
+			}
+		}
+		// works
 		for (int q = cx; q < cx + sizeX; q++) {
 			for (int w = cy; w < cy + sizeY; w++) {
 				for (int e = 0; e < cz; e++) {
-					if(WorldGenerator.inBounds(q,w,e)){
-						WorldGenerator.data[q][w][e] = 100;
+					if (WorldGenerator.inBounds(q, w, e)) {
+						WorldGenerator.data[q][w][e] = Block.setBlock(e,
+								WorldGenerator.mTracker[q][w]);
+
 					}
 				}
 			}
 		}
 	}
+
 	private static void kinikariRuinsLeast(int cx, int cy, int cz) {
 		sizeX = 11;
 		sizeY = 6;
 		sizeZ = 10;
-		clear(cx, cy, cz);
+
 		if (cx + sizeX < Map.HEXESACROSS && cy + sizeY < Map.HEXESDOWN
 				&& cz + sizeZ < Map.WORLDHEIGHT && cx % 2 == 0) {
+			clear(cx, cy, cz);
 			for (int z = cz; z < cz + 5; z++) {
 				WorldGenerator.data[cx + 5][cy][z] = Block.KINIKARI_WALL;
 				WorldGenerator.data[cx + 4][cy][z] = Block.KINIKARI_WALL;
 				WorldGenerator.data[cx + 6][cy][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx + 3][cy+1][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx + 7][cy+1][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx + 2][cy+1][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx + 8][cy+1][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx + 1][cy+2][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx + 9][cy+2][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx][cy+2][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx + 10][cy+2][z] = Block.KINIKARI_WALL;
-				
-				WorldGenerator.data[cx + 1][cy+3][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx + 9][cy+3][z] = Block.KINIKARI_WALL;
-				
-				WorldGenerator.data[cx + 3][cy+3][z] = Block.KINIKARI_WALL;
-				WorldGenerator.data[cx + 7][cy+3][z] = Block.KINIKARI_WALL;
-				
-				WorldGenerator.data[cx + 5][cy+3][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx + 3][cy + 1][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx + 7][cy + 1][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx + 2][cy + 1][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx + 8][cy + 1][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx + 1][cy + 2][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx + 9][cy + 2][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx][cy + 2][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx + 10][cy + 2][z] = Block.KINIKARI_WALL;
+
+				WorldGenerator.data[cx + 1][cy + 3][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx + 9][cy + 3][z] = Block.KINIKARI_WALL;
+
+				WorldGenerator.data[cx + 3][cy + 3][z] = Block.KINIKARI_WALL;
+				WorldGenerator.data[cx + 7][cy + 3][z] = Block.KINIKARI_WALL;
+
+				WorldGenerator.data[cx + 5][cy + 3][z] = Block.KINIKARI_WALL;
 			}
-			
-			WorldGenerator.data[cx + 5][cy+3][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 5][cy+2][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 5][cy+1][cz+4] = Block.KINIKARI_WALL;
-			
-			
-			WorldGenerator.data[cx + 4][cy+2][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 4][cy+1][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 3][cy+3][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 3][cy+2][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 2][cy+2][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 1][cy+3][cz+4] = Block.KINIKARI_WALL;
-			
-			WorldGenerator.data[cx + 6][cy+2][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 6][cy+1][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 7][cy+3][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 7][cy+2][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 8][cy+2][cz+4] = Block.KINIKARI_WALL;
-			WorldGenerator.data[cx + 9][cy+3][cz+4] = Block.KINIKARI_WALL;
-			
-			
-			
-			
-			WorldGenerator.data[cx + 5][cy+4][cz+2] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+4][cz+2] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+4][cz+2] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+4][cz+2] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+4][cz+2] = Block.KINIKARI_ROOF;
-			
-			WorldGenerator.data[cx + 5][cy+5][cz+3] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+5][cz+3] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+5][cz+3] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+5][cz+3] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+5][cz+3] = Block.KINIKARI_ROOF;
-			
-			WorldGenerator.data[cx + 5][cy+4][cz+3] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+4][cz+3] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+4][cz+3] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+4][cz+3] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+4][cz+3] = Block.KINIKARI_ROOF;
-			
-			WorldGenerator.data[cx + 5][cy+5][cz+4] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+5][cz+4] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+5][cz+4] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+5][cz+4] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+5][cz+4] = Block.KINIKARI_ROOF;
-			
-			WorldGenerator.data[cx + 5][cy+5][cz+5] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+5][cz+5] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+5][cz+5] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+5][cz+5] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+5][cz+5] = Block.KINIKARI_ROOF;
-			
-			WorldGenerator.data[cx + 5][cy+4][cz+5] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+4][cz+5] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+4][cz+5] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+4][cz+5] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+4][cz+5] = Block.KINIKARI_ROOF;
-			
-			WorldGenerator.data[cx + 5][cy+4][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+4][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+4][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+4][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+4][cz+6] = Block.KINIKARI_ROOF;
-			
-			WorldGenerator.data[cx + 5][cy+3][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+3][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+3][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+3][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+3][cz+6] = Block.KINIKARI_ROOF;
-			
-			WorldGenerator.data[cx + 5][cy+3][cz+7] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+3][cz+7] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+3][cz+7] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+3][cz+7] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+3][cz+7] = Block.KINIKARI_ROOF;
-			
-			WorldGenerator.data[cx + 5][cy+2][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 3][cy+2][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 7][cy+2][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 1][cy+2][cz+6] = Block.KINIKARI_ROOF;
-			WorldGenerator.data[cx + 9][cy+2][cz+6] = Block.KINIKARI_ROOF;
-			
-			
+
+			WorldGenerator.data[cx + 5][cy + 3][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 5][cy + 2][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 5][cy + 1][cz + 4] = Block.KINIKARI_WALL;
+
+			WorldGenerator.data[cx + 4][cy + 2][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 4][cy + 1][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 3][cy + 3][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 3][cy + 2][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 2][cy + 2][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 1][cy + 3][cz + 4] = Block.KINIKARI_WALL;
+
+			WorldGenerator.data[cx + 6][cy + 2][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 6][cy + 1][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 7][cy + 3][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 7][cy + 2][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 8][cy + 2][cz + 4] = Block.KINIKARI_WALL;
+			WorldGenerator.data[cx + 9][cy + 3][cz + 4] = Block.KINIKARI_WALL;
+
+			WorldGenerator.data[cx + 5][cy + 4][cz + 2] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 4][cz + 2] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 4][cz + 2] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 4][cz + 2] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 4][cz + 2] = Block.KINIKARI_ROOF;
+
+			WorldGenerator.data[cx + 5][cy + 5][cz + 3] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 5][cz + 3] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 5][cz + 3] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 5][cz + 3] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 5][cz + 3] = Block.KINIKARI_ROOF;
+
+			WorldGenerator.data[cx + 5][cy + 4][cz + 3] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 4][cz + 3] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 4][cz + 3] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 4][cz + 3] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 4][cz + 3] = Block.KINIKARI_ROOF;
+
+			WorldGenerator.data[cx + 5][cy + 5][cz + 4] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 5][cz + 4] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 5][cz + 4] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 5][cz + 4] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 5][cz + 4] = Block.KINIKARI_ROOF;
+
+			WorldGenerator.data[cx + 5][cy + 5][cz + 5] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 5][cz + 5] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 5][cz + 5] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 5][cz + 5] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 5][cz + 5] = Block.KINIKARI_ROOF;
+
+			WorldGenerator.data[cx + 5][cy + 4][cz + 5] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 4][cz + 5] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 4][cz + 5] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 4][cz + 5] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 4][cz + 5] = Block.KINIKARI_ROOF;
+
+			WorldGenerator.data[cx + 5][cy + 4][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 4][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 4][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 4][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 4][cz + 6] = Block.KINIKARI_ROOF;
+
+			WorldGenerator.data[cx + 5][cy + 3][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 3][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 3][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 3][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 3][cz + 6] = Block.KINIKARI_ROOF;
+
+			WorldGenerator.data[cx + 5][cy + 3][cz + 7] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 3][cz + 7] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 3][cz + 7] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 3][cz + 7] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 3][cz + 7] = Block.KINIKARI_ROOF;
+
+			WorldGenerator.data[cx + 5][cy + 2][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 3][cy + 2][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 7][cy + 2][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 1][cy + 2][cz + 6] = Block.KINIKARI_ROOF;
+			WorldGenerator.data[cx + 9][cy + 2][cz + 6] = Block.KINIKARI_ROOF;
+
 		}
 
 	}
 
 	private static void jeneunGateLeast(int cx, int cy, int cz) {
-		// TODO Auto-generated method stub
+		sizeX = 10;
+		sizeY = 10;
+		sizeZ = 10;
+		if (cx + sizeX < Map.HEXESACROSS && cy + sizeY < Map.HEXESDOWN
+				&& cz + sizeZ < Map.WORLDHEIGHT && cx % 2 == 0) {
+			clear(cx, cy, cz);
+			for (int x = cx; x < cx + 11; x++) {
+				for (int y = cy; y < cy + 10; y++) {
+					WorldGenerator.data[x][y][cz] = Block.JENEUN_WALL;
+				}
+			}
+		}
 
 	}
 
@@ -180,9 +213,10 @@ public class Structure {
 		sizeX = 10;
 		sizeY = 10;
 		sizeZ = 10;
-		clear(cx, cy, cz);
+
 		if (cx + sizeX < Map.HEXESACROSS && cy + sizeY < Map.HEXESDOWN
 				&& cz + sizeZ < Map.WORLDHEIGHT && cx % 2 == 0) {
+			clear(cx, cy, cz);
 			for (int x = cx; x < cx + 11; x++) {
 				for (int y = cy; y < cy + 10; y++) {
 					WorldGenerator.data[x][y][cz + 5] = Block.JENEUN_WALL;
@@ -224,7 +258,6 @@ public class Structure {
 					WorldGenerator.data[x + 2][y + 2][cz + 10] = Block.JENEUN_ROOF;
 				}
 			}
-			
 
 		}
 
@@ -236,9 +269,10 @@ public class Structure {
 		sizeX = 9;
 		sizeY = 7;
 		sizeZ = 7;
-		clear(cx, cy, cz);
+
 		if (cx + sizeX < Map.HEXESACROSS && cy + sizeY < Map.HEXESDOWN
 				&& cz + sizeZ < Map.WORLDHEIGHT && cx % 2 == 0) {
+			clear(cx, cy, cz);
 			for (int z = cz; z < cz + 5; z++) {
 				WorldGenerator.data[cx + 5][cy + 1][z] = Block.OCTO_WALL;
 				WorldGenerator.data[cx + 4][cy + 1][z] = Block.OCTO_WALL;
@@ -292,7 +326,6 @@ public class Structure {
 			WorldGenerator.data[cx + 5][cy + 5][cz + 6] = Block.OCTO_ROOF;
 
 			WorldGenerator.data[cx + 5][cy + 4][cz + 7] = Block.OCTO_ROOF;
-
 
 		}
 	}
